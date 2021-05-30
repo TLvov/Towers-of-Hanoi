c108539886ab59a200c00b59b358cfac6d496117
Tihon Lvov <84801181+TLvov@users.noreply.github.com>
2021-05-30 19:53:00 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace TLvov_TowersOfHanoi_Ver2
+{
+    partial class frmTowersOfHanoi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sldTowers = new System.Windows.Forms.TrackBar();
+            this.picBoxTowerDisplay = new System.Windows.Forms.PictureBox();
+            this.sldDisks = new System.Windows.Forms.TrackBar();
+            this.lblNumOfTowers = new System.Windows.Forms.Label();
+            this.lblNumOfDisks = new System.Windows.Forms.Label();
+            this.lblThree_1 = new System.Windows.Forms.Label();
+            this.lblFour_1 = new System.Windows.Forms.Label();
+            this.lblFive_1 = new System.Windows.Forms.Label();
+            this.lblThree_2 = new System.Windows.Forms.Label();
+            this.lblFour_2 = new System.Windows.Forms.Label();
+            this.lblFive_2 = new System.Windows.Forms.Label();
+            this.lblSix = new System.Windows.Forms.Label();
+            this.lblSeven = new System.Windows.Forms.Label();
+            this.txtGameInfo = new System.Windows.Forms.TextBox();
+            this.cmdRestart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.sldTowers)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxTowerDisplay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sldDisks)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sldTowers
+            // 
+            this.sldTowers.Location = new System.Drawing.Point(170, 388);
+            this.sldTowers.Maximum = 5;
+            this.sldTowers.Minimum = 3;
+            this.sldTowers.Name = "sldTowers";
+            this.sldTowers.Size = new System.Drawing.Size(270, 45);
+            this.sldTowers.TabIndex = 3;
+            this.sldTowers.Value = 3;
+            this.sldTowers.ValueChanged += new System.EventHandler(this.sldTowers_ValueChanged);
+            // 
+            // picBoxTowerDisplay
+            // 
+            this.picBoxTowerDisplay.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.picBoxTowerDisplay.Location = new System.Drawing.Point(56, 12);
+            this.picBoxTowerDisplay.Name = "picBoxTowerDisplay";
+            this.picBoxTowerDisplay.Size = new System.Drawing.Size(850, 360);
+            this.picBoxTowerDisplay.TabIndex = 2;
+            this.picBoxTowerDisplay.TabStop = false;
+            this.picBoxTowerDisplay.MouseDown += new System.Windows.Forms.MouseEventHandler(this.picBoxTowerDisplay_MouseDown);
+            this.picBoxTowerDisplay.MouseMove += new System.Windows.Forms.MouseEventHandler(this.picBoxTowerDisplay_MouseMove);
+            this.picBoxTowerDisplay.MouseUp += new System.Windows.Forms.MouseEventHandler(this.picBoxTowerDisplay_MouseUp);
+            // 
+            // sldDisks
+            // 
+            this.sldDisks.Location = new System.Drawing.Point(170, 470);
+            this.sldDisks.Maximum = 7;
+            this.sldDisks.Minimum = 3;
+            this.sldDisks.Name = "sldDisks";
+            this.sldDisks.Size = new System.Drawing.Size(270, 45);
+            this.sldDisks.TabIndex = 4;
+            this.sldDisks.Value = 3;
+            this.sldDisks.ValueChanged += new System.EventHandler(this.sldDisks_ValueChanged);
+            // 
+            // lblNumOfTowers
+            // 
+            this.lblNumOfTowers.AutoSize = true;
+            this.lblNumOfTowers.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumOfTowers.Location = new System.Drawing.Point(53, 388);
+            this.lblNumOfTowers.Name = "lblNumOfTowers";
+            this.lblNumOfTowers.Size = new System.Drawing.Size(111, 15);
+            this.lblNumOfTowers.TabIndex = 5;
+            this.lblNumOfTowers.Text = "Number of Towers:";
+            // 
+            // lblNumOfDisks
+            // 
+            this.lblNumOfDisks.AutoSize = true;
+            this.lblNumOfDisks.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumOfDisks.Location = new System.Drawing.Point(63, 470);
+            this.lblNumOfDisks.Name = "lblNumOfDisks";
+            this.lblNumOfDisks.Size = new System.Drawing.Size(101, 15);
+            this.lblNumOfDisks.TabIndex = 6;
+            this.lblNumOfDisks.Text = "Number of Disks:";
+            // 
+            // lblThree_1
+            // 
+            this.lblThree_1.AutoSize = true;
+            this.lblThree_1.ForeColor = System.Drawing.Color.DimGray;
+            this.lblThree_1.Location = new System.Drawing.Point(177, 420);
+            this.lblThree_1.Name = "lblThree_1";
+            this.lblThree_1.Size = new System.Drawing.Size(13, 13);
+            this.lblThree_1.TabIndex = 7;
+            this.lblThree_1.Text = "3";
+            // 
+            // lblFour_1
+            // 
+            this.lblFour_1.AutoSize = true;
+            this.lblFour_1.ForeColor = System.Drawing.Color.DimGray;
+            this.lblFour_1.Location = new System.Drawing.Point(300, 420);
+            this.lblFour_1.Name = "lblFour_1";
+            this.lblFour_1.Size = new System.Drawing.Size(13, 13);
+            this.lblFour_1.TabIndex = 8;
+            this.lblFour_1.Text = "4";
+            // 
+            // lblFive_1
+            // 
+            this.lblFive_1.AutoSize = true;
+            this.lblFive_1.ForeColor = System.Drawing.Color.DimGray;
+            this.lblFive_1.Location = new System.Drawing.Point(420, 420);
+            this.lblFive_1.Name = "lblFive_1";
+            this.lblFive_1.Size = new System.Drawing.Size(13, 13);
+            this.lblFive_1.TabIndex = 9;
+            this.lblFive_1.Text = "5";
+            // 
+            // lblThree_2
+            // 
+            this.lblThree_2.AutoSize = true;
+            this.lblThree_2.ForeColor = System.Drawing.Color.DimGray;
+            this.lblThree_2.Location = new System.Drawing.Point(177, 502);
+            this.lblThree_2.Name = "lblThree_2";
+            this.lblThree_2.Size = new System.Drawing.Size(13, 13);
+            this.lblThree_2.TabIndex = 10;
+            this.lblThree_2.Text = "3";
+            // 
+            // lblFour_2
+            // 
+            this.lblFour_2.AutoSize = true;
+            this.lblFour_2.ForeColor = System.Drawing.Color.DimGray;
+            this.lblFour_2.Location = new System.Drawing.Point(237, 502);
+            this.lblFour_2.Name = "lblFour_2";
+            this.lblFour_2.Size = new System.Drawing.Size(13, 13);
+            this.lblFour_2.TabIndex = 11;
+            this.lblFour_2.Text = "4";
+            // 
+            // lblFive_2
+            // 
+            this.lblFive_2.AutoSize = true;
+            this.lblFive_2.ForeColor = System.Drawing.Color.DimGray;
+            this.lblFive_2.Location = new System.Drawing.Point(300, 502);
+            this.lblFive_2.Name = "lblFive_2";
+            this.lblFive_2.Size = new System.Drawing.Size(13, 13);
+            this.lblFive_2.TabIndex = 12;
+            this.lblFive_2.Text = "5";
+            // 
+            // lblSix
+            // 
+            this.lblSix.AutoSize = true;
+            this.lblSix.ForeColor = System.Drawing.Color.DimGray;
+            this.lblSix.Location = new System.Drawing.Point(359, 502);
+            this.lblSix.Name = "lblSix";
+            this.lblSix.Size = new System.Drawing.Size(13, 13);
+            this.lblSix.TabIndex = 13;
+            this.lblSix.Text = "6";
+            // 
+            // lblSeven
+            // 
+            this.lblSeven.AutoSize = true;
+            this.lblSeven.ForeColor = System.Drawing.Color.DimGray;
+            this.lblSeven.Location = new System.Drawing.Point(420, 502);
+            this.lblSeven.Name = "lblSeven";
+            this.lblSeven.Size = new System.Drawing.Size(13, 13);
+            this.lblSeven.TabIndex = 14;
+            this.lblSeven.Text = "7";
+            // 
+            // txtGameInfo
+            // 
+            this.txtGameInfo.Location = new System.Drawing.Point(630, 387);
+            this.txtGameInfo.Multiline = true;
+            this.txtGameInfo.Name = "txtGameInfo";
+            this.txtGameInfo.ReadOnly = true;
+            this.txtGameInfo.Size = new System.Drawing.Size(276, 139);
+            this.txtGameInfo.TabIndex = 15;
+            // 
+            // cmdRestart
+            // 
+            this.cmdRestart.Location = new System.Drawing.Point(463, 387);
+            this.cmdRestart.Name = "cmdRestart";
+            this.cmdRestart.Size = new System.Drawing.Size(150, 65);
+            this.cmdRestart.TabIndex = 16;
+            this.cmdRestart.Text = "Restart";
+            this.cmdRestart.UseVisualStyleBackColor = true;
+            this.cmdRestart.Click += new System.EventHandler(this.cmdRestart_Click);
+            // 
+            // frmTowersOfHanoi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(990, 550);
+            this.Controls.Add(this.cmdRestart);
+            this.Controls.Add(this.txtGameInfo);
+            this.Controls.Add(this.lblSeven);
+            this.Controls.Add(this.lblSix);
+            this.Controls.Add(this.lblFive_2);
+            this.Controls.Add(this.lblFour_2);
+            this.Controls.Add(this.lblThree_2);
+            this.Controls.Add(this.lblFive_1);
+            this.Controls.Add(this.lblFour_1);
+            this.Controls.Add(this.lblThree_1);
+            this.Controls.Add(this.lblNumOfDisks);
+            this.Controls.Add(this.lblNumOfTowers);
+            this.Controls.Add(this.sldDisks);
+            this.Controls.Add(this.sldTowers);
+            this.Controls.Add(this.picBoxTowerDisplay);
+            this.Name = "frmTowersOfHanoi";
+            this.Text = "Towers of Hanoi";
+            ((System.ComponentModel.ISupportInitialize)(this.sldTowers)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxTowerDisplay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sldDisks)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar sldTowers;
+        private System.Windows.Forms.PictureBox picBoxTowerDisplay;
+        private System.Windows.Forms.TrackBar sldDisks;
+        private System.Windows.Forms.Label lblNumOfTowers;
+        private System.Windows.Forms.Label lblNumOfDisks;
+        private System.Windows.Forms.Label lblThree_1;
+        private System.Windows.Forms.Label lblFour_1;
+        private System.Windows.Forms.Label lblFive_1;
+        private System.Windows.Forms.Label lblThree_2;
+        private System.Windows.Forms.Label lblFour_2;
+        private System.Windows.Forms.Label lblFive_2;
+        private System.Windows.Forms.Label lblSix;
+        private System.Windows.Forms.Label lblSeven;
+        private System.Windows.Forms.TextBox txtGameInfo;
+        private System.Windows.Forms.Button cmdRestart;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TLvov_TowersOfHanoi_Ver2
+{
+    public partial class frmTowersOfHanoi : Form
+    {
+        //Global Variable Assingment
+        int[,] disks;
+        int[] towerPointer;
+        string[] feedback = { "", "You must place the disk onto a tower!", "Disks cannot be placed on disks larger than themselves!", "You did it! Good job!" };
+        SolidBrush[] p = new SolidBrush[7] { new SolidBrush(Color.Red), new SolidBrush(Color.Orange), new SolidBrush(Color.Gold), new SolidBrush(Color.YellowGreen), new SolidBrush(Color.DarkCyan), new SolidBrush(Color.RoyalBlue), new SolidBrush(Color.BlueViolet)};
+        int numOfTowers = 3, numOfDisks = 3, tower = 0, diskHeld, attempts = 0, feedbackInt;
+        bool movable = true;
+
+        private void picBoxTowerDisplay_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (movable == true)
+            {
+                MouseEventArgs me = (MouseEventArgs)e;
+                int x = me.X;   //Gets x coordinate of cursor in relation to the picture box
+
+                foreach (int i in Enumerable.Range(1, numOfTowers + 1)) //Finds tower that the x coordinate matches
+                {
+                    if (x <= 67 + i * (50 + numOfDisks * 16) - (numOfDisks * 8) & numOfTowers >= i)
+                    {
+                        tower = i;
+                        break;  //Stops searching once relevant tower has been found
+                    }
+                }
+                if (tower != 0)
+                {
+                    if (towerPointer[tower - 1] != 0)
+                    {
+                        PopStack(tower - 1);    //"Pops" stack of relevant tower
+                        Display(0);             //Redraws the display
+                        var bmp = picBoxTowerDisplay.Image;
+                        using (var flagGraphics = Graphics.FromImage(bmp))
+                        {
+                            flagGraphics.FillRectangle(p[diskHeld - 1], me.X - 25, me.Y - 15, (diskHeld + 5) * 10, 30); //Draws the disk where the cursor is (within the picture box)
+                            picBoxTowerDisplay.Image = bmp;
+                        }
+                    }
+                    else
+                    {
+                        tower = 0;
+                    }
+                }
+            }
+        }
+
+        private void picBoxTowerDisplay_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (movable == true)
+            {
+                int num = 0;
+                if (tower != 0) //Wont do anything unless you're actually holding a disk
+                {
+                    MouseEventArgs me = (MouseEventArgs)e;
+                    int x = me.X;
+                    foreach (int i in Enumerable.Range(1, numOfTowers + 1)) //Finds tower that the x coordinate matches
+                    {
+                        if (x <= 67 + i * (50 + numOfDisks * 16) - (numOfDisks * 8) & numOfTowers >= i)
+                        {
+                            num = i;    
+                            break;  //Stops searching once relevant tower has been found
+                        }
+                    }
+                    if (num != 0)
+                    {
+                        if (num == tower)
+                        {
+                            PushStack(tower - 1);   //"Pushes" stack of relevant tower
+                            attempts--;             //Removes an attempt as it shouldn't count as one
+                            feedbackInt = 0;
+                        }
+                        else if (towerPointer[num - 1] == 0)
+                        {
+                            PushStack(num - 1); //"Pushes" stack of relevant tower
+                            feedbackInt = 0;
+                        }
+                        else if (diskHeld < disks[num - 1, towerPointer[num - 1] - 1] || towerPointer[num - 1] == 0)
+                        {
+                            PushStack(num - 1); //"Pushes" stack of relevant tower
+                            feedbackInt = 0;
+                        }
+                        else
+                        {
+                            PushStack(tower - 1);   //"Pushes" stack of relevant tower
+                            attempts--;             //Removes an attempt as it shouldn't count as one
+                            feedbackInt = 2;
+                        }
+                    }
+                    else
+                    {
+                        PushStack(tower - 1);   //"Pushes" stack of relevant tower
+                        attempts--;             //Removes an attempt as it shouldn't count as one
+                        feedbackInt = 1;
+                    }
+                    Display(0); //Redraws the display
+                    attempts++; //Increases the attempt counter
+                    sldTowers.Enabled = false;  //Disables sliders
+                    sldDisks.Enabled = false;   //
+                    tower = 0;
+                }
+            }
+        }
+
+        private void picBoxTowerDisplay_MouseMove(object sender, MouseEventArgs e)
+        {
+            MouseEventArgs me = (MouseEventArgs)e;
+            int x = me.X;   //Gets x coordinate of cursor in relation to the picture box
+            int n = 0;
+            if (movable == true)
+            {
+                foreach (int i in Enumerable.Range(1, numOfTowers + 1)) //Finds tower that the x coordinate matches
+                {
+                    if (x <= 67 + i * (50 + numOfDisks * 16) - (numOfDisks * 8) & numOfTowers >= i)
+                    {
+                        n = i;
+                        break;  //Stops searching once relevant tower has been found
+                    }
+                }
+                Display(n); //Redraws the display, this time will show a darkened tower (the one the user is going to affect)
+            }
+            if (tower != 0)
+            {
+                var bmp = picBoxTowerDisplay.Image;
+                using (var flagGraphics = Graphics.FromImage(bmp))
+                {
+                    flagGraphics.FillRectangle(p[diskHeld - 1], me.X - 25, me.Y - 15, (diskHeld + 5) * 10, 30); //Draws the disk where the cursor is (within the picture box)
+                    picBoxTowerDisplay.Image = bmp;
+                }
+            }
+        }
+
+        private void PopStack(int tower)
+        {
+            towerPointer[tower]--;  //Decreases the pointer by 1
+            diskHeld = disks[tower, towerPointer[tower]];   //Sets the value of the disk held to the disk at relevant location
+            disks[tower, towerPointer[tower]] = 0;  //Removes disk of relavent location from the tower
+        }
+
+        private async void PushStack(int tower)
+        {
+            movable = false;    //Disables player action whilst disk pushing is happening
+            cmdRestart.Enabled = false;
+            for (int i = 0; i < (numOfDisks - towerPointer[tower]) * 40; i = i + 5) //Determines how many times the disk will be drawn
+            {
+                Display(0); //Redraws the display
+                var bmp = picBoxTowerDisplay.Image;
+                int x = tower;
+                using (var flagGraphics = Graphics.FromImage(bmp))
+                {
+                    flagGraphics.FillRectangle(p[diskHeld - 1], 67 + tower * (50 + numOfDisks * 16) - diskHeld * 5, 185 + i - (numOfDisks - 3) * 40, (diskHeld + 5) * 10, 30);
+                    picBoxTowerDisplay.Image = bmp; //Draws a disk on top of display at relevant location, this repeats many times forming an animation
+                }
+                await Task.Delay(1);    //One millisecond pause between each frame
+            }
+            Console.Beep(); //Plays beep sound when disk "lands"
+            disks[tower, towerPointer[tower]] = diskHeld;   //Sets the value of the relevant location in the tower to that of the disk held
+            towerPointer[tower]++;  //Increases the pointer by 1
+            if (towerPointer[numOfTowers - 1] == numOfDisks)    //Checks if the player has beaten the puzzle
+            {
+                movable = false;
+                cmdRestart.Enabled = false;
+                feedbackInt = 3;
+                Display(0);
+            }
+            else
+            {
+                movable = true; //If player has not beaten the puzzle, they are now allowed to make another action
+                cmdRestart.Enabled = true;
+            }
+        }
+
+        private void sldTowers_ValueChanged(object sender, EventArgs e)
+        {
+            numOfTowers = sldTowers.Value;  //Changes the amount of towers
+            Load();
+        }
+
+        private void sldDisks_ValueChanged(object sender, EventArgs e)
+        {
+            numOfDisks = sldDisks.Value;    //Changes the amount of disks
+            Load();
+        }
+
+        private async void cmdRestart_Click(object sender, EventArgs e)
+        {
+            attempts = 0;               //Resets all important values and reloads the program, waits for a millisecond and then plays a beep noise
+            feedbackInt = 0;
+            movable = true;
+            Load();
+            sldDisks.Enabled = true;
+            sldTowers.Enabled = true;
+            await Task.Delay(1);
+            Console.Beep();
+        }
+
+        private new void Load()
+        {
+            int[,] setDisks = new int[numOfTowers, numOfDisks]; //Creates a new array with dimensions based on the amounts of towers and disks
+            foreach (int i in Enumerable.Range(0, numOfTowers))
+            {
+                foreach (int k in Enumerable.Range(0, numOfDisks))
+                {
+                    if (i == 0)
+                    {
+                        setDisks[i, k] = numOfDisks - k;    //Sets all elements of the first tower to their relevant values
+                    }
+                    else
+                    {
+                        setDisks[i, k] = 0; //Sets all other elements to zero
+                    }
+                }
+            }
+            disks = setDisks;   //Sets the global array to the new array
+            int[] setTowerPointer = new int[numOfTowers];   //Creates a new array of pointers based on the amount of towers
+            foreach (int i in Enumerable.Range(0, numOfTowers))
+            {
+                setTowerPointer[i] = 0; //Sets all elements of the array to zero
+            }
+            setTowerPointer[0] = numOfDisks;    //Sets the pointer of the first tower to the amount of disks
+            towerPointer = setTowerPointer; //Sets the global array to the new array
+            Display(0); //Redraws the display
+        }
+
+        public void Display(int tower)
+        {
+            Controls.Add(picBoxTowerDisplay);
+            Bitmap flag = new Bitmap(picBoxTowerDisplay.Width, picBoxTowerDisplay.Height);
+            Graphics flagGraphics = Graphics.FromImage(flag);
+            SolidBrush p1 = new SolidBrush(Color.DarkGray);
+            SolidBrush p2 = new SolidBrush(Color.DimGray);
+            Rectangle[] rectsTowers = new Rectangle[numOfTowers];
+            foreach (int i in Enumerable.Range(0, numOfTowers))
+            {
+                rectsTowers[i] = new Rectangle(80 + i * (50 + numOfDisks * 16), 330 - 40 * numOfDisks, 25, numOfDisks * 40);
+            }
+            flagGraphics.FillRectangles(p1, rectsTowers);   //Draws all towers
+            if (tower != 0)
+            {
+                flagGraphics.FillRectangle(p2, 80 + (tower - 1) * (50 + numOfDisks * 16), 330 - 40 * numOfDisks, 25, numOfDisks * 40);  //Draws darker tower if player is about to affect said tower
+            }
+            foreach (int i in Enumerable.Range(0, numOfTowers))
+            {
+                foreach (int k in Enumerable.Range(0, numOfDisks))
+                {
+                    if (disks[i, k] != 0)
+                    {
+                        flagGraphics.FillRectangle(p[disks[i, k] - 1], 67 + i * (50 + numOfDisks * 16) - disks[i, k] * 5, 300 - k * 40, (disks[i, k] + 5) * 10, 30);    //Draws all disks in relevant colours and locations
+                    }
+                }
+            }
+            picBoxTowerDisplay.Image = flag;
+            string newLine = Environment.NewLine;
+            txtGameInfo.Text = "Bring all disks from the first tower to the last tower!" + newLine + feedback[feedbackInt] + newLine + newLine + "Number of towers = " + numOfTowers + newLine + "Number of disks = " + numOfDisks + newLine + newLine + newLine + "Attempts: " + attempts;
+        }
+
+        public frmTowersOfHanoi()
+        {
+            InitializeComponent();
+            Load();
+        }
+    }
+}
